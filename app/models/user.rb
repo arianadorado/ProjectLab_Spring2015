a9fd54e97eb411fc2ff55d5a20859177a685ed07
Carlos Garcia <carlos.garcia@carlosagp.com>
2015-04-13 11:49:24 -0600

    Added User.from_omniauth and User.new_with_session

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,27 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, :omniauth_providers => [:github]
+
+  def self.from_omniauth(auth)
+    # binding.pry 
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      # logger.debug('before pry')
+      # user.email = auth.info.email
+      user.first_name = auth.info.name.split(' ').first
+      user.last_name = auth.info.name.split(' ').last
+      user.password = user.password_confirmation = Devise.friendly_token[0,20]
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.provider = session["devise.github_data"]['provider']
+        user.uid = session["devise.github_data"]['uid']
+        user.first_name =  data["name"].split(' ').first
+        user.last_name = data["name"].split(' ').last
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
